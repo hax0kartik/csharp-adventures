8b4d2fba5284a1ce11924e68bde224cd9085424a
PratyutCS <pratyut987@gmail.com>
2022-06-14 09:34:15 +0530

    photos added

--- tictactoe/tictactoe/Game1.cs
+++ tictactoe/tictactoe/Game1.cs
@@ -1,6 +1,7 @@
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
 using System.Collections.Generic;
 using System;
 using System.Diagnostics;
@@ -16,13 +17,16 @@ namespace tictactoe
         int[] a = { 0, 210, 420 };
         int[] board = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         Vector2[] boardcoords;
-        Vector2 coor;
         Color[] data;
+        Texture2D image,image1;
+        List<SoundEffect> soundEffects;
+        Rectangle img;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            soundEffects = new List<SoundEffect>();
         }
 
         protected override void Initialize()
@@ -42,24 +46,31 @@ namespace tictactoe
 
             /* Generate Board Coords */
             boardcoords = new Vector2[9];
-            for(int i = 0; i < 9; i++){
+            for (int i = 0; i < 9; i++)
+            {
                 int row = i / 3;
                 boardcoords[i].X = a[row];
                 boardcoords[i].Y = a[i - (row * 3)];
             }
+            image = Content.Load<Texture2D>("254-2545026_delete-button-nor-white-cross-icon-png");
+            image1 = Content.Load<Texture2D>("download");
+            img.X = 0;
+            img.Y = 0;
+            img.Width = 200;
+            img.Height = 200;            
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            soundEffects.Add(Content.Load<SoundEffect>("mixkit-medieval-show-fanfare-announcement-226"));
+                        // TODO: use this.Content to load your game content here
         }
 
         int counter = 0;
         bool CheckWin(int option)
         {
-            for(int i = 0; i < 3; i++)
+            for (int i = 0; i < 3; i++)
             {
                 /* Check rows */
                 if (board[i] == option && board[i + 3] == option && board[i + 6] == option)
@@ -78,12 +89,10 @@ namespace tictactoe
 
         bool CheckTie()
         {
-            int i = 0;
-            while(i < 9)
+            for ( int i=0 ; i < 9; i++ )
             {
                 if (board[i] == 0)
                     return false;
-                i++;
             }
             return true;
         }
@@ -97,25 +106,27 @@ namespace tictactoe
             x = mouse.Position.X;
             y = mouse.Position.Y;
 
-            if (mouse.LeftButton == ButtonState.Pressed && counter > 30)
+            if (mouse.LeftButton == ButtonState.Pressed && counter > 10)
             {
-                for(int i = 0; i < 9; i++)
+                for (int i = 0; i < 9; i++)
                 {
                     if (x > boardcoords[i].X && x < boardcoords[i].X + 200.0f
                         && y > boardcoords[i].Y && y < boardcoords[i].Y + 200.0f
                         && board[i] == 0)
                     {
-                        int player =  c ? 2 : 1;
+                        int player = c ? 2 : 1;
                         board[i] = player;
                         c = !c;
                         if (CheckWin(player))
                         {
                             Debug.WriteLine("Player " + player + " Won!");
+                            /*soundEffects[0].CreateInstance().Play();*/
                             Exit();
                         }
                         else if (CheckTie())
                         {
                             Debug.WriteLine("Its a Tie!");
+                            /*soundEffects[0].CreateInstance().Play();*/
                             Exit();
                         }
                     }
@@ -129,19 +140,23 @@ namespace tictactoe
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
-            
-            for(int i = 0; i < 9; i++)
+
+            for (int i = 0; i < 9; i++)
             {
-                switch(board[i])
+                switch (board[i])
                 {
                     case 0:
-                        _spriteBatch.Draw(rect, boardcoords[i], Color.Gray);
+                        _spriteBatch.Draw(rect, boardcoords[i], Color.Green);
                         break;
                     case 1:
-                        _spriteBatch.Draw(rect, boardcoords[i], Color.Blue);
+                        img.X = (int) boardcoords[i].X;
+                        img.Y = (int) boardcoords[i].Y;
+                        _spriteBatch.Draw(image, img, Color.White);
                         break;
                     case 2:
-                        _spriteBatch.Draw(rect, boardcoords[i], Color.Green);
+                        img.X = (int)boardcoords[i].X;
+                        img.Y = (int)boardcoords[i].Y;
+                        _spriteBatch.Draw(image1, img, Color.White);
                         break;
                 }
             }
